10a92168b764c59a0e64c66b8eca518c5c851d16
Andy Calderbank <andy@plumhead-software.com>
2016-04-04 19:42:42 +0100

    Created basic Cocoa XCode project starting point

---
+++ CocoaTextAttachmentCells/CocoaTextAttachmentCells/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaTextAttachmentCells
+//
+//  Created by Plumhead on 04/04/2016.
+//  Copyright © 2016 Plumhead Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CocoaTextAttachmentCells/CocoaTextAttachmentCells/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaTextAttachmentCells
+//
+//  Created by Plumhead on 04/04/2016.
+//  Copyright © 2016 Plumhead Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
